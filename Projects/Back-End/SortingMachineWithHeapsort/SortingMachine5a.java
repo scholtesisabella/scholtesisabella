80996b91ff9c4a66b23b19c2d42930cfddb6bbc0
scholtesisabella <isabellascholtes@outlook.com>
2024-10-16 16:58:43 -0400

    Update SortingMachine5a.java

--- Projects/Back-End/SortingMachineWithHeapsort/SortingMachine5a.java
+++ Projects/Back-End/SortingMachineWithHeapsort/SortingMachine5a.java
@@ -60,7 +60,7 @@ import components.sortingmachine.SortingMachineSecondary;
  *   this = (false, $this.machineOrder, multiset_entries($this.heap[0, $this.heapSize)))
  * </pre>
  *
- * @author Bella and Akshaya
+ * @author Bella Scholtes
  *
  */
 public class SortingMachine5a<T> extends SortingMachineSecondary<T> {
